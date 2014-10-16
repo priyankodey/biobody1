8b52033ffcecc4e48e0053620e4aa5e3b3c58ea0
priyankodey <priyankodey87@gmail.com>
2014-10-16 18:10:09 +0530

    First commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def product
+  end
+
+  def contact
+  end
+
+  def help
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biobody::Application.config.secret_key_base = '79ae555ab7c519aaaede5e3fd7ad4a9138a98b105cf216d838c1293f7176bd9351c7df289b36b8aa56d754c2f89f1a5f6b9ea1447d50b175625318ea5afba4b9'
